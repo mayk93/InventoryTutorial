f2be0f386277d815ff7cd6e9af20eeba6e7687cc
mayk93 <mihai.mandrescu@gmail.com>
2015-04-04 22:54:24 +0300

    Implemented basic layout
    Basic layout.

--- InventoryTutorial/Assets/Scripts/Inventory.cs
+++ InventoryTutorial/Assets/Scripts/Inventory.cs
@@ -31,14 +31,31 @@ public class Inventory : MonoBehaviour
 
 	private void CreateLayout()
 	{
+		int columns = slots / rows;
+
 		allSlots = new List<GameObject>();
 
-		inventoryWidth = (slots / rows) * (slotSize + slotPaddingLeft) + slotPaddingLeft;
+		inventoryWidth = columns * (slotSize + slotPaddingLeft) + slotPaddingLeft;
 		inventoryHeight = rows * (slotSize + slotPladdingTop) + slotPladdingTop;
 
 		inventoryRect = GetComponent<RectTransform> ();
 
 		inventoryRect.SetSizeWithCurrentAnchors (RectTransform.Axis.Horizontal, inventoryWidth);
 		inventoryRect.SetSizeWithCurrentAnchors (RectTransform.Axis.Vertical, inventoryHeight);
+
+		for (int y = 0; y<rows; y++) 
+		{
+			for(int x = 0; x<columns; x++)
+			{
+				GameObject newSlot = (GameObject)Instantiate(slotPrefab);
+				RectTransform slotRect = newSlot.GetComponent<RectTransform>();
+				newSlot.name = "Slot";
+				newSlot.transform.SetParent(this.transform.parent);
+				slotRect.localPosition = inventoryRect.localPosition + new Vector3( (slotPaddingLeft * (x + 1) + (slotSize * x)) , (-slotPladdingTop * (y+1) - (slotSize * y)) , 1 );
+				slotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal,slotSize);
+				slotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,slotSize);
+				allSlots.Add(newSlot);
+			}
+		}
 	}
 }
