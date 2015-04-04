11cfbcfc49f9c07cc1882d02824e5a816d0ceb99
mayk93 <mihai.mandrescu@gmail.com>
2015-04-04 22:42:46 +0300

    Inventory Tutorial
    Unity inventory tutorial. To extend to IP project.

---
+++ InventoryTutorial/Assets/Scripts/Inventory.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour 
+{
+	private RectTransform inventoryRect;
+
+	private float inventoryWidth;
+	private float inventoryHeight;
+
+	public int slots;
+	public int rows;
+
+	public float slotPaddingLeft;
+	public float slotPladdingTop;
+
+	public float slotSize;
+
+	public GameObject slotPrefab;
+
+	private List<GameObject> allSlots;
+	// Use this for initialization
+	void Start () 
+	{
+		CreateLayout ();
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+
+	private void CreateLayout()
+	{
+		allSlots = new List<GameObject>();
+
+		inventoryWidth = (slots / rows) * (slotSize + slotPaddingLeft) + slotPaddingLeft;
+		inventoryHeight = rows * (slotSize + slotPladdingTop) + slotPladdingTop;
+
+		inventoryRect = GetComponent<RectTransform> ();
+
+		inventoryRect.SetSizeWithCurrentAnchors (RectTransform.Axis.Horizontal, inventoryWidth);
+		inventoryRect.SetSizeWithCurrentAnchors (RectTransform.Axis.Vertical, inventoryHeight);
+	}
+}
